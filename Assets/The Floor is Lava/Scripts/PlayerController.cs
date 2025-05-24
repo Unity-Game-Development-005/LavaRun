08f71fc3d79f22c9b63749a8ea815536445f5fe6
Andrew W. F. Hatton <7912262+Wanottah@users.noreply.github.com>
2025-05-24 23:31:42 +0100

    Added UI text
    Completed maze construction and added 'YOU WIN' UI text

--- Assets/The Floor is Lava/Scripts/PlayerController.cs
+++ Assets/The Floor is Lava/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,9 +13,12 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
 
     private float movementX;
+
     private float movementY;
 
     private Vector3 startPosition;
+
+    public Canvas uiCanvas;
 
     
 
@@ -22,8 +26,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        startPosition = transform.position;
-        
+
+        startPosition = transform.position;       
     }
 
 
@@ -31,35 +35,56 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        Move();
-       
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            uiCanvas.gameObject.SetActive(true);
+        }
+
+        Move();     
     }
+
 
     private void Move()
     {
         movementX = Input.GetAxis("Horizontal");
+
         movementY = Input.GetAxis("Vertical");
     }
+
 
     private void FixedUpdate()
     {
         PlayerMovement();
     }
 
+
     private void PlayerMovement()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        Vector3 movement = new Vector3(-movementX, 0.0f, -movementY);
 
         rb.AddForce(movement.normalized * speed);
     }
 
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Lava")
+        if (other.tag == "Lava")
         {
             transform.position = startPosition;
+
             rb.linearVelocity = Vector3.zero;
         }
     }
 
-}
+    private void OnCollision(Collision other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            rb.linearVelocity = Vector3.zero;
+
+            uiCanvas.gameObject.SetActive(true);
+        }
+    }
+
+
+} // end of class
